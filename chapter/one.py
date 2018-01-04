dbb8a42c4a5e74522688b0cd41d1e8d02ed348c0
jeffrey-cochran <koenigcochran@gmail.com>
2018-01-04 17:19:23 -0500

    continue to update algorithms from ch1

--- chapter/one.py
+++ chapter/one.py
@@ -1,3 +1,4 @@
+from numpy import zeros, asarray
 
 
 def Horner1(a, u0):
@@ -14,3 +15,80 @@ def Horner1(a, u0):
         C = C * u0 + a[i]
     #
     return C
+
+
+def Bernstein(i, n, u):
+    '''Calculate the Bernstein polynomial, B_i,n(u), at u'''
+    #
+    # Initialize temp array
+    temp = zeros((1, n + 1))
+    temp[n - i] = 1.0
+    #
+    # Compute 1 less u
+    u0 = 1 - u
+    #
+    # Compute the columns of Table 1.1, p. 20
+    # Outer loop iterates over all degrees < n
+    # families of Bernstein polynomials
+    for k in range(1, n + 1):
+        #
+        # Inner loop computes current value of 
+        # Bernstein polynomial family based on
+        # previous family's evaluation.
+        for j in range(n, k - 1, -1):
+            temp[j] = u0 * temp[j] + u * temp[j - 1]
+        #
+    #
+    # Last entry is value of polynomial at u
+    B = temp[n]
+    #
+    return B
+
+
+def AllBernstein(n, u):
+    #
+    B_list = zeros((1, n + 1))
+    B_list[0] = 1.0
+    u0 = 1.0 - u
+    for j in range(1, n + 1):
+        saved = 0
+        for k in range(0, j):
+            temp = B_list[k]
+            B_list[k] = saved + u0 * temp
+            saved = u * temp
+        #
+        B_list[j] = saved
+        #
+    #
+    return B_list
+
+
+def PointOnBezierCurve(P, n, u):
+    #
+    B_list = AllBernstein(n, u)
+    C = asarray([0.0, 0.0])
+    for k in range(0, n + 1): C += (B_list[k] * P[k])
+    #
+    return C
+
+
+def deCasteljau1(P, n, u):
+    #
+    Q = tuple(point.copy() for point in P)
+    u0 = 1.0 - u
+    #
+    for k in range(1, n + 1):
+        for i in range(0, n - k + 1):
+            Q[i] = u0 * Q[i] + u * Q[i + 1]
+        #
+    #
+    C = Q[0]
+    #
+    return C
+
+
+def Horner2(a, n, m, u0, v0):
+    #
+    
+    #
+    return S
