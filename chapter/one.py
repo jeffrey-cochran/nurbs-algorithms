c1e5c4d8bd65dd81b0d1888a82a5c071ef278048
jeffrey-cochran <koenigcochran@gmail.com>
2018-01-04 14:29:48 -0500

    init ch1

---
+++ chapter/one.py
@@ -0,0 +1,16 @@
+
+
+def Horner1(a, u0):
+    '''Horner algorithm for computing a power basis curve'''
+    #
+    # Get degree
+    degree = len(a) - 1
+    #
+    # Initialize curve evaluation
+    C = a[degree]
+    #
+    # Iterate over lower exponents
+    for i in range(degree - 1, 0, -1):
+        C = C * u0 + a[i]
+    #
+    return C
